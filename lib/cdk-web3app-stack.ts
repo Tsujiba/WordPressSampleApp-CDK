a55b947468c5b9a2c093f841b06884a967bdaa94
Ubuntu <ubuntu@ip-172-31-39-206.ap-northeast-1.compute.internal>
2023-12-06 10:54:20 +0000

    construct create

--- lib/cdk-web3app-stack.ts
+++ lib/cdk-web3app-stack.ts
@@ -1,10 +1,10 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { readFileSync } from 'fs';
 import * as ec2 from 'aws-cdk-lib/aws-ec2'
 import * as rds from 'aws-cdk-lib/aws-rds'
 import * as elbv2 from "aws-cdk-lib/aws-elasticloadbalancingv2";
 import * as targets from "aws-cdk-lib/aws-elasticloadbalancingv2-targets";
+import { WebServerInstance } from './constructs/web-server-instance';
 
 export class CdkWeb3AppStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -15,17 +15,11 @@ export class CdkWeb3AppStack extends Stack {
       ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16')
     })
 
-    // EC2の定義
-    const webserver1 = new ec2.Instance(this, 'WebServer', {
-      vpc,
-      vpcSubnets:{ subnetType: ec2.SubnetType.PUBLIC},
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.SMALL),
-      machineImage: new ec2.AmazonLinuxImage({generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,}),
-    })
 
-    // ユーザデータスクリプトの定義
-    const script = readFileSync("./lib/resources/user-data.sh", "utf8");
-    webserver1.addUserData(script);
+    // EC2の定義
+    const webserver1 = new WebServerInstance(this, 'WebServer1', {
+      vpc
+    })
 
     // RDS for MySQLの定義
     const dbinstance = new rds.DatabaseInstance(this, "MySQLDB", {
@@ -33,10 +27,11 @@ export class CdkWeb3AppStack extends Stack {
       engine: rds.DatabaseInstanceEngine.mysql({version: rds.MysqlEngineVersion.VER_8_0_31}),
       instanceType: ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.SMALL),
       databaseName: "wordpress",
+      // multiAz: true,
     });
 
     // DBの通信をEC2へ限定
-    dbinstance.connections.allowDefaultPortFrom(webserver1);
+    dbinstance.connections.allowDefaultPortFrom(webserver1.instance);
 
     // ALBの定義
     const alb = new elbv2.ApplicationLoadBalancer(this, "LoadBalancer", {
@@ -52,14 +47,14 @@ export class CdkWeb3AppStack extends Stack {
     // インスタンスをターゲットを追加
     listener.addTargets("ApplicationFleet", {
       port: 80,
-      targets: [new targets.InstanceTarget(webserver1, 80)],
+      targets: [new targets.InstanceTarget(webserver1.instance, 80)],
       healthCheck: {
         path: "/wp-includes/images/blank.gif",
       }
     });
 
     // ALBからEC2へアクセス許可
-    webserver1.connections.allowFrom(alb, ec2.Port.tcp(80));
+    webserver1.instance.connections.allowFrom(alb, ec2.Port.tcp(80));
 
   }
 }
